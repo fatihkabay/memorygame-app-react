4109a1207d3143cc1d3904d3cd2ca99f695292ed
Fatih Kabay <mfthkabay@gmail.com>
2022-08-18 15:36:05 +0300

    index error in code operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css';
-import Images from './Images.js';
+import Images from './Images';
 import {useState} from "react";
 import {shuffle} from 'loadsh';
+import { initial } from 'lodash';
 
 function App() {
   const [cards,setCards] = useState( shuffle([...Images, ...Images]) );
+  const [activeCards, setActiveCards] = useState({initialState : []});
+
+  function flipCard(index) {
+     setActiveCards({value : [index]})
+  }
 
   return (
     <div>
       <div className="board">
         {cards.map(({card,index : number})=> (
-            <div className="card-outer">
+            <div className={"card-outer" + ((activeCards.indexOf(index) !== -1) ? 'flipped' : '')}  onClick={() => flipCard(index)}>
           <div className="card">
             <div className="front">
               <img src={card} alt="" />
