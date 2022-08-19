e22201c48b8abc2a18542643b4d6b678d99d4733
Fatih Kabay <mfthkabay@gmail.com>
2022-08-19 21:33:06 +0300

    click event finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ import {shuffle} from 'loadsh';
 
 function App() {
   const [cards,setCards] = useState( shuffle([...Images, ...Images]) );
+  const [click, setClick] = useState({initialState : [0]});
   const [activeCards, setActiveCards] = useState({initialState : []});
-  const [foundMatches, setFoundMatches] = useState({initialState: []});
+  const [foundPairs, setFoundPairs] = useState({initialState: []});
 
   function flipCard(index) {
     if (activeCards.length === 0) {
       setActiveCards({value : [index]});
     }
     if (activeCards.length === 1) {
+      const firstIndex = activeCards[0];
+      const secondsIndex = index;
+      if (cards[firstIndex] === cards[secondsIndex]) {
+        setFoundPairs ({value : [...foundPairs, firstIndex, secondsIndex]})
+    }
       setActiveCards({value:[...activeCards, index]});
     }
    if (activeCards.length === 2) {
-    const firstIndex = activeCards[0];
-    const secondsIndex = activeCards[1];
-    
-    if (cards[firstIndex] === cards[secondsIndex]) {
-      alert('Your found a pair')
-    }
     
     setActiveCards({value : [index]});
    }
@@ -32,17 +32,20 @@ function App() {
   return (
     <div>
       <div className="board">
-        {cards.map((card,index : number)=> (
-            <div className={"card-outer" + ((activeCards.indexOf(index) !== -1) ? 'flipped' : '')}  onClick={() => flipCard(index)}>
-          <div className="card">
-            <div className="front">
-              <img src={card} alt="" />
-            </div>
-            <div className="back"></div>
+        {cards.map((card,index : number)=> { 
+           const flippedToFront = (activeCards.indexOf(index) !== -1) || foundPairs.indexOf(index) !== -1;
+           return ( 
+           <div className={"card-outer" + (flippedToFront ? 'flipped' : '')}  onClick={() => flipCard(index)}>
+           <div className="card">
+             <div className="front">
+               <img src={card} alt="" />
+             </div>
+             <div className="back"></div>
+           </div>
           </div>
-         </div>
-      
-        ))}
+       );
+
+         })}
        </div>
     </div>
   );
